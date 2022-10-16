18dfb7ae218b9a067cfa9619987f31aede6a4600
Patrick Cuppi <cuppi.patrick@gmail.com>
2022-10-16 15:07:13 -0300

    Migração do HTML dos componentes restantes.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import Home from "./components/Home";
+//import Home from "./components/pages/Home";
+//import UserBlog from "./components/pages/UserBlog";
+//import Users from "./components/pages/Users";
+import CreatePost from "./components/pages/CreatePost";
+
 import "./styles/normalize.css";
 import "./styles/main.css";
 import "./styles/fontawesome.min.css";
 
 function App() {
-  return <Home />;
+  return <CreatePost />;
 }
 
 export default App;

b9479ad88310f0b6b6f058462d6c61f67b655d11
Patrick Cuppi <cuppi.patrick@gmail.com>
2022-10-17 14:07:25 -0300

    Adiciona roteamento (react-router-dom)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-//import Home from "./components/pages/Home";
-//import UserBlog from "./components/pages/UserBlog";
-//import Users from "./components/pages/Users";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./components/pages/Home";
+import UserBlog from "./components/pages/UserBlog";
+import Users from "./components/pages/Users";
 import CreatePost from "./components/pages/CreatePost";
 
 import "./styles/normalize.css";
@@ -8,7 +10,16 @@ import "./styles/main.css";
 import "./styles/fontawesome.min.css";
 
 function App() {
-  return <CreatePost />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/users/:userId" element={<UserBlog />} />
+        <Route path="/users/posts/new" element={<CreatePost />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
